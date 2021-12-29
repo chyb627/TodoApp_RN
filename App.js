64535324b8628d0cf77b8a240051fb23d17eeb34
cha <chyb627@gmail.com>
2021-12-29 16:02:03 +0900

    추가 : Empty화면 추가

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
+
 import DateHead from './components/DateHead';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
+
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 function App ()  {
@@ -10,15 +14,20 @@ function App ()  {
 
   return (
       <SafeAreaProvider>
-        <SafeAreaView edges={['bottom']}>
+        <SafeAreaView edges={['bottom']} style={styles.block}>
           <DateHead date={today} />
+          <Empty />
+          <AddTodo />
         </SafeAreaView>
       </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
 });
 
 export default App;
---
+++ components/Empty.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+function Empty() {
+
+    const source = {uri: 'https://via.placeholder.com/150'}
+
+    return(
+        <View style={styles.block} >
+            <Image 
+                // source={require('../assets/images/circle.png')}
+                // source={source}
+                source={require('../assets/images/young_and_happy.png')}
+                style={styles.image}
+                resizeMode="contain"
+             />
+            <Text style={styles.description}>할일이 없습니다</Text>        
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  block: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  description: {
+      fontSize: 24,
+      color: '#9e9e9e'
+  },
+  image: {
+    // width: 300,
+    // height: 200,
+    // backgroundColor: 'gray',
+    width: 240,
+    height: 179,
+    marginBottom: 16,
+  }
+});
+
+export default Empty;
